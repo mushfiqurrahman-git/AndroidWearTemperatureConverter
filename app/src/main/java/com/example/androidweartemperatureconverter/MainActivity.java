a9ec833893543d55def98f2be6d3977711a33a70
mushfiqurahman-git <mymensingh456@gmail.com>
2024-09-25 18:02:19 -0400

    done

--- app/src/main/java/com/example/androidweartemperatureconverter/MainActivity.java
+++ app/src/main/java/com/example/androidweartemperatureconverter/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.androidweartemperatureconverter;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,42 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.androidweartemperatureconverter.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = activityMainBinding.getRoot();
+
+        setContentView(view);
+
+        activityMainBinding.button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String input = String.valueOf(Integer.parseInt(String.valueOf(activityMainBinding.textView14.getText())));
+                //int C = (input-32)
+                Integer intInput = Integer.parseInt(input);
+                Integer C = ((intInput - 32) * 5) / 9;
+                String result = C.toString()+" F";
+
+                activityMainBinding.textView21.setText(result);
+            }
+        });
+        activityMainBinding.button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String input = String.valueOf(Integer.parseInt(String.valueOf(activityMainBinding.textView14.getText())));
+
+                Integer intInput = Integer.parseInt(input);
+                Integer F = ((intInput * 9) / 5) + 32;
+                String result = F.toString()+" C";
+                activityMainBinding.textView21.setText(result);
+            }
         });
     }
 }
