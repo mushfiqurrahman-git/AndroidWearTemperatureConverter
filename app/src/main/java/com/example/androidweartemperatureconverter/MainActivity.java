8307913e36aee4cf91a08e6208d1a113c36e11e2
mushfiqurahman-git <mymensingh456@gmail.com>
2024-09-25 19:00:56 -0400

    all done with validation

--- app/src/main/java/com/example/androidweartemperatureconverter/MainActivity.java
+++ app/src/main/java/com/example/androidweartemperatureconverter/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.androidweartemperatureconverter;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -13,39 +14,61 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.androidweartemperatureconverter.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    //use of view binding
     ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //inflating the layout thru view binding
         activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        //getting to the root
         View view = activityMainBinding.getRoot();
-
+        //setting the view
         setContentView(view);
-
+        
+        //onclick listener for C
         activityMainBinding.button7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //converting the input to Sting value
+                try {
+                    String input = String.valueOf(Integer.parseInt(String.valueOf(activityMainBinding.textView14.getText())));
+                    //parsing to int
+                    Integer intInput = Integer.parseInt(input);
+                    //calculation
+                    Integer C = ((intInput - 32) * 5) / 9;
+                    //storing the result as string
+                    String result = C.toString();
+                    //showing the result in result section
+                    activityMainBinding.textView21.setText(result);
+                    //changing the TEXT based on btn
+                    activityMainBinding.textView2.setText("F°");
+                }catch (NumberFormatException exception)
+                {//showing Toast if there is no input given
+                    Toast.makeText(MainActivity.this, "Enter input to get F°", Toast.LENGTH_SHORT).show();
+                }
 
-                String input = String.valueOf(Integer.parseInt(String.valueOf(activityMainBinding.textView14.getText())));
-                //int C = (input-32)
-                Integer intInput = Integer.parseInt(input);
-                Integer C = ((intInput - 32) * 5) / 9;
-                String result = C.toString()+" F";
-
-                activityMainBinding.textView21.setText(result);
             }
         });
+        
+        
+        //onclick listener for F
         activityMainBinding.button8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String input = String.valueOf(Integer.parseInt(String.valueOf(activityMainBinding.textView14.getText())));
-
-                Integer intInput = Integer.parseInt(input);
-                Integer F = ((intInput * 9) / 5) + 32;
-                String result = F.toString()+" C";
-                activityMainBinding.textView21.setText(result);
+                try {
+                    String input = String.valueOf(Integer.parseInt(String.valueOf(activityMainBinding.textView14.getText())));
+                    Integer intInput = Integer.parseInt(input);
+                    Integer F = ((intInput * 9) / 5) + 32;
+                    String result = F.toString();
+                    activityMainBinding.textView21.setText(result);
+                    activityMainBinding.textView2.setText("C°");
+                } catch (NumberFormatException exception) {
+                    Toast.makeText(MainActivity.this, "Enter input to get C°", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+
     }
 }
